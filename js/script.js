8284d577593edf9c3ef073d3108b32a4762b20eb
yusef02 <youssef.bouziane2402@gmail.com>
2024-01-08 17:07:19 +0100

    script(): aggiunto numero 24 alla pwd

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let userSurname = prompt(`inserisci il tuo cognome: `);
 console.log(userSurname);
 let userColor = prompt(`inserisci il tuo colore: `);
 console.log(userColor);
-let generatedPwd = userName + userSurname + userColor;
+let generatedPwd = userName + userSurname + userColor + 24;
+// ho inserito il 24 come un semplice numero perche in questo caso trattandosi di una varibile stringa non occore forzatamente trasformare i numeri in stringhe
 console.log(generatedPwd);
 
 const mainTitle = document.createElement('h1');
